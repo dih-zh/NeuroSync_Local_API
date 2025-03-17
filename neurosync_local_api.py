e506b78708de6ec089c336302ecfd0ce17e7252d
Steven Wolf <steven.wolf+git@pm.me>
2025-03-17 15:16:04 +0100

    migrate to json payload because of googles vertex ai endpoints

--- neurosync_local_api.py
+++ neurosync_local_api.py
@@ -6,6 +6,7 @@ from flask import request, jsonify
 import numpy as np
 import torch
 import flask
+import base64
 
 from utils.generate_face_shapes import generate_facial_data_from_bytes
 from utils.model.model import load_model
@@ -20,19 +21,30 @@ model_path = "utils/model/model.pth"
 blendshape_model = load_model(model_path, config, device)
 
 
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "ok"})
+
+
 @app.route("/audio_to_blendshapes", methods=["POST"])
 def audio_to_blendshapes_route():
-    audio_bytes = request.data
-    generated_facial_data = generate_facial_data_from_bytes(
-        audio_bytes, blendshape_model, device, config
-    )
-    generated_facial_data_list = (
-        generated_facial_data.tolist()
-        if isinstance(generated_facial_data, np.ndarray)
-        else generated_facial_data
-    )
+    print(request.json)
+    if request.json:
+        audio_bytes = request.json["audio"]
+        audio_bytes = base64.b64decode(audio_bytes)
 
-    return jsonify({"blendshapes": generated_facial_data_list})
+        generated_facial_data = generate_facial_data_from_bytes(
+            audio_bytes, blendshape_model, device, config
+        )
+        generated_facial_data_list = (
+            generated_facial_data.tolist()
+            if isinstance(generated_facial_data, np.ndarray)
+            else generated_facial_data
+        )
+
+        return jsonify({"blendshapes": generated_facial_data_list})
+
+    return jsonify({"error": "bad payload"})
 
 
 if __name__ == "__main__":
